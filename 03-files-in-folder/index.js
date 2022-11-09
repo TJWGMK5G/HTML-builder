898f46471eb0eb58b9536f439897390a1100103f
TJWGMK5G <87062165+TJWGMK5G@users.noreply.github.com>
2022-11-09 14:05:39 +0300

    fix: new code
    changes code node js

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,7 +3,6 @@ const path = require('path')
 fs.readdir('secret-folder', (err, data) => {
     console.log(data);
     data.forEach( file => {
-        console.log(file+" "+path.extname(file));
-        console.log(fs.statSync("secret-folder/"+file).size+" "+'byte');
+        console.log(file+" "+ "-" +" "+path.extname(file)+" "+"-"+" "+(fs.statSync("secret-folder/"+file).size+" "+'byte'));
     });
 });
